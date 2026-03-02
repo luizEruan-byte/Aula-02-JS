1b94bab13203df7d702940e3be1287660a9978fb
Luiz Ruan <luizeruan2026@gmail.com>
2026-03-02 11:22:55 -0300

    02 - Operadores relacionais e lógicos com JS

--- js/app.js
+++ js/app.js
@@ -1,19 +1,50 @@
 let primeiroNumero = parseInt(Math.random()*900);
 let segundoNumero = parseInt(Math.random()*500);
-let resultado = primeiroNumero + segundoNumero;
+let soma = primeiroNumero + segundoNumero;
 console.log(primeiroNumero, segundoNumero); 
-console.log(resultado);
-let antecessor = resultado;
-console.log(resultado-1)
-let sucessor = antecessor;
-console.log(antecessor+1)
-let dobro =  resultado;
-console.log(resultado*2);
-let divisao =  resultado;
-console.log(resultado/2);
+console.log(soma);
+
+let subtracao = primeiroNumero - segundoNumero;
+console.log(subtracao);
+let multiplicacao = primeiroNumero * segundoNumero;
+console.log(multiplicacao);
+let divisao = primeiroNumero / segundoNumero;
+console.log(divisao);
+let modulo = primeiroNumero % segundoNumero;
+console.log(modulo);
+let igualdade = primeiroNumero === segundoNumero;
+console.log(igualdade);
+let desigualdade = primeiroNumero !== segundoNumero;
+console.log(desigualdade);
+let maiorigual = primeiroNumero >= segundoNumero;
+console.log(maiorigual);
+let menorigual = primeiroNumero <= segundoNumero;
+console.log(menorigual);
+let maior = primeiroNumero > segundoNumero;
+console.log(maior);
+let menor = primeiroNumero <= segundoNumero;
+console.log(menor);
+
 
 // exibindo a página para isso criamos uma variavel que recebe o elemento html 
 document.getElementById("elPrimeiroNumero").textContent = `Primeiro Número: ${primeiroNumero}`;
 document.getElementById("elSegundoNumero").textContent = `Segundo Número: ${segundoNumero}`;
-document.getElementById("elresultadoSoma").textContent = `resultado: ${resultado}`;
-`${primeiroNumero} + ${segundoNumero} = ${resultado}`;
+document.getElementById("elResultadoSoma").textContent = `Primeiro número + Segundo Número: ${soma}`;
+`${primeiroNumero} + ${segundoNumero} = ${soma}`;
+document.getElementById("elResultadoSubtracao").textContent = `Primeiro Número - Segundo Número: ${subtracao}`;
+document.getElementById("elResultadoMultiplicacao").textContent = `Primeiro Número * Segundo Número: ${multiplicacao}`;
+document.getElementById("elResultadoDivisao").textContent = `Primeiro Número / Segundo Número: ${divisao.toFixed(2)}`;
+document.getElementById("elIgualdade").textContent = `Primeiro Número = Segundo Número: ${igualdade}`;
+document.getElementById("elDesigualdade").textContent = `Primeiro Número ! Segundo Número: ${desigualdade}`;
+document.getElementById("elMaiorIgual").textContent = `Primeiro Número >= Segundo Número: ${maiorigual}`;
+document.getElementById("elMenorIgual").textContent = `Primeiro Número <= Segundo Número: ${menorigual}`;
+document.getElementById("elMaior").textContent = `Primeiro Número > Segundo Número: ${maior}`;
+document.getElementById("elMenor").textContent = `Primeiro Número < Segundo Número: ${menor}`;
+
+
+resultado = (primeiroNumero >= segundoNumero) || (segundoNumero > 3);
+document.getElementById('operadorOU').textContent = `${primeiroNumero} > ${segundoNumero} || ${segundoNumero} > 3 = ${resultado}`;
+resultado = (primeiroNumero >= segundoNumero) && (segundoNumero > 3);
+document.getElementById('operadorE').textContent = `${primeiroNumero} > ${segundoNumero} && ${segundoNumero} > 3 = ${resultado}`;
+resultado = !((primeiroNumero >= segundoNumero) && (segundoNumero > 3));
+document.getElementById('operadorNAO').textContent = `!(${primeiroNumero} > ${segundoNumero} || ${segundoNumero} > 3) = ${resultado}`;
